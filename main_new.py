800ed580f004e401c9e27846606f5f2595ec088f
Harsh Sinha <harsh@Harshs-MacBook-Pro.local>
2023-11-06 12:09:24 -0800

    working acetametophine funcitonality to the point of obtaining routes.

--- main_new.py
+++ main_new.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, jsonify
 import requests
 import json
+from functools import lru_cache
 
 app = Flask(__name__)
 
@@ -8,40 +9,65 @@ app = Flask(__name__)
 def index():
     return render_template("index_new.html")
 
+
+@lru_cache(maxsize=32)
+def get_drug_suggestions(query):
+    url = f"https://api.drugbank.com/v1/product_concepts?q={query}"
+    headers = {
+        'authorization': 'df338cac4d1c76de487260acb441be5c'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        suggestion_list = []
+        for item in data:
+            for hit in item.get('hits', []):
+                # Remove HTML tags for highlighting
+                clean_value = hit['value'].replace('<em>', '').replace('</em>', '')
+                suggestion_list.append({
+                'value': clean_value,  # the display value
+                'data': {
+                    'value': clean_value,
+                    'drugbank_pcid': item['drugbank_pcid']  # the PCID from the item
+                }
+            })
+        return suggestion_list
+    else:
+        response.raise_for_status()
+
+
 @app.route("/search/<string:box>")
 def process(box):
     query = request.args.get('query')
+    if box == 'names' and len(query) >= 3:
+        try:
+            suggestions = get_drug_suggestions(query)
+        except requests.HTTPError as e:
+            # Handle HTTP errors here
+            return jsonify({"error": str(e)}), e.response.status_code
+        return jsonify({"suggestions": suggestions})
+    else:
+        # Handle cases where query is too short or box is not 'names'
+        return jsonify({"error": "Invalid request"})
 
-    if box == 'names':
-        print("input data= ",query)
-        url = f"https://api.drugbank.com/v1/drug_names?q={query}"
 
-        payload = {}
-        headers = {
-        'authorization': 'df338cac4d1c76de487260acb441be5c'
-        }
 
-        response = requests.request("GET", url, headers=headers, data=payload)
-        val = json.loads(response.text)
-        # print(len(val['products']))
-        suggestion_list = []
-        for i in range(len(val['products'])):
-            for j in range(len(val['products'][i]['hits'])):
-                print(val['products'][i]['hits'][j]['value'])
+@app.route("/drug_routes/<string:pcid>")
+def drug_routes(pcid):
+    url = f"https://api.drugbank.com/v1/product_concepts/{pcid}/routes"
+    headers = {
+        'authorization': 'df338cac4d1c76de487260acb441be5c'  # Make sure to keep your API keys secret in a production environment
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        routes_list = [{'label': f"{item.get('name')}", 'value': item.get('route')} for item in data]
+        return jsonify(routes_list)
+    else:
+        return jsonify({"error": "Failed to fetch drug routes"}), response.status_code
 
-                # do some stuff to open your names text file
-                # do some other stuff to filter
-                # put suggestions in this format...
-                {'value': val['products'][i]['hits'][j]['value'],'data': val['products'][i]['hits'][j]['value']}
-                suggestion_list.append({'value': val['products'][i]['hits'][j]['value'],'data': val['products'][i]['hits'][j]['value']})
-        # suggestions = [{'value': 'joe','data': 'joe'},{'value': 'aman','data': 'aman'}, {'value': 'jim','data': 'jim'}]
-        suggestions = suggestion_list
-    # if box == 'songs':
-    #     # do some stuff to open your songs text file
-    #     # do some other stuff to filter
-    #     # put suggestions in this format...
-    #     suggestions = [{'value': 'song1','data': '123'}, {'value': 'song2','data': '234'}]
-    return jsonify({"suggestions":suggestions})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
